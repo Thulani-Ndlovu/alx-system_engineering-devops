77003c564a0c1be826b2092b69d4c02b96d69e81
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-04 23:10:47 +0200

    Task 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
     Script for a given employee ID, returns information
     about his/her TODO list progress
 '''
-import sys
 import requests
+import sys
 
 if __name__ == '__main__':
     apiURL = "https://jsonplaceholder.typicode.com/"
