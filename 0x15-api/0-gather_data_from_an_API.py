a4f3b02ce26e2dd14ef5fced8d426172a371f40a
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-04 23:05:01 +0200

    Task 0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+    Script for a given employee ID, returns information
+    about his/her TODO list progress
+'''
+import sys
+import requests
+
+if __name__ == '__main__':
+    apiURL = "https://jsonplaceholder.typicode.com/"
+    User = requests.get(apiURL + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(apiURL +
+                         "todos", params={"userId: sys.argv[1]"}).json()
+    Completed = [t.get('title') for t in todos if t.get('completed') is True]
+    print("Employee {} is done with tasks({}/{})".format(
+        User.get("name"), len(Completed), len(todos)
+    ))
+    [print("\t {}".format(i)) for i in Completed]
