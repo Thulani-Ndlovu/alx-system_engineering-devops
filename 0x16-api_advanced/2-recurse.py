546986ce9bc433502f71f98d59c9b59c101c69bf
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-08 01:34:45 +0200

    Task 2. Recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    recursive function that queries the Reddit API
+    and returns a list containing the titles of all
+    hot articles for a given subreddit
+'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    '''recursively query reddit api'''
+    apiURL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Thulani_Ndlovu"
+    }
+    params = {
+        "count": count,
+        "after": after
+    }
+    res = requests.get(apiURL, headers=headers, params=params,
+                       allow_redirects=False)
+    if res.status_code == 404:
+        return None
+    result = res.json().get("data")
+    after = res.get("after")
+    count += res.get("dist")
+    for i in res.get("children"):
+        hot_list.append(i.get("data").get("title"))
+    if after is not None:
+        return recurse(subreddit, hot_list, count, after)
+    return hot_list
