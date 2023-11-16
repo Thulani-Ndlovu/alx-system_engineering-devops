1b7ce0e5a771bf6bee413ee0fa00f987b926ec1d
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-16 21:25:25 +0200

    Task 1. User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Changing OS Config to login with holberton and open a file with error
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# increase limit for soft file
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
