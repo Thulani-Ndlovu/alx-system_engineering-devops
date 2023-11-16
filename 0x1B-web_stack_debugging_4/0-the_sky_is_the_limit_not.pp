e6b572adc2c2176d82540b95ac8a25d3cdb1e8a0
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-11-16 21:14:03 +0200

    Task 0. Sky is the limit, lets bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Fixing server request errors
 
 exec { 'fix--for-nginx':
-  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path     => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restarting Nginx
 exec { 'nginx-restart':
-  command  => 'nginx restart',
-  path     => '/etc/init.d/'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
